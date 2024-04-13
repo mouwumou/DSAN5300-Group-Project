fa7df6d27a998a4c204c356ed70d5db5941b297f
mouwumou <mouwumou@gmail.com>
2024-04-12 22:33:43 -0400

    modify project structure

---
+++ code/download_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# download data from the State of Washington "Electric Vehicle Population Data"
+# https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2
+# and save it to data/raw/ folder
+
+import os
+import requests
+
+current_file_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_file_path))
+
+data_raw_folder = os.path.join(project_root, 'data', 'raw')
+
+# create data/raw/ folder
+if not os.path.exists(data_raw_folder):
+    os.makedirs(data_raw_folder)
+
+# download data
+data_file_path = os.path.join(data_raw_folder, 'Electric_Vehicle_Population_Data.csv')
+
+url = r'https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD'
+r = requests.get(url)
+with open(data_file_path, 'wb') as f:
+    f.write(r.content)
+
+print('Data downloaded and saved to {}'.format(data_file_path))
